a88e68dcb61b53d54c039a69fc4a9a2683d63cc9
wshicheng <630030437@qq.com>
2017-08-23 15:44:19 +0800

    modify

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@
 // 阿里云服务器
 export const host = 'http://47.93.48.250:8090/'
 // 海波
-// export const host = 'http://192.168.3.85:8080/'
+// export const host = 'http://192.168.0.114:8080/'
 // 沈灿
 // export const host = 'http://192.168.0.106:8080/'
 // 孙威
